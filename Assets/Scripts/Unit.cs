b9dc0930a3b876faa4a5113798d9e203224e1c0d
PseudoscienceGames <quade.81@gmail.com>
2019-02-05 19:12:33 -0600

    Init files

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+	public string myName;
+	public string myClass;
+	public string myLevel;
+	public int[] stats;
+	public Squad squad;
+
+	private void Start()
+	{
+		string letters = "abcdefghijklmnopqrstuvwxyz";
+		int length = Random.Range(3, 11);
+		for (int i = 0; i < length; i++)
+		{
+			myName += letters[Random.Range(0, letters.Length)];
+			myClass += letters[Random.Range(0, letters.Length)];
+		}
+		myLevel = Random.Range(0, 30).ToString();
+	}
+}
