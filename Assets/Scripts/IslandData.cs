4e46e06d1fd3ddee833a38d72f4a2d1d84dae11a
PseudoscienceGames <quade.81@gmail.com>
2019-05-03 21:38:15 -0500

    starting height calc

--- Assets/Scripts/IslandData.cs
+++ Assets/Scripts/IslandData.cs
@@ -8,4 +8,15 @@ public class IslandData
 	public Dictionary<Vector2Int, WorldTile> tiles = new Dictionary<Vector2Int, WorldTile>();
 	public int tileCount;
 
+	public void CalcHeights()
+	{
+		List<Vector3> verts = new List<Vector3>();
+		foreach(WorldTile w in tiles.Values)
+		{
+			foreach(Vector3 v in w.verts)
+			{
+				here
+			}
+		}
+	}
 }
